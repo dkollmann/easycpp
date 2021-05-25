37c040b3ef732c4531365f022a7e493e0d91cc7f
Daniel Kollmann <dankolle@mail.de>
2021-05-25 21:04:17 +0200

    Added the simple project as a template.

---
+++ templates/gdnative/src/Simple.hpp
@@ -0,0 +1,28 @@
+#include <Godot.hpp>
+
+#include <Reference.hpp>
+#include <Sprite.hpp>
+
+class Simple : public godot::Reference {
+	GODOT_CLASS(Simple, godot::Reference)
+
+	godot::String data;
+
+public:
+	static void _register_methods();
+
+	void _init();
+
+	godot::String get_data() const;
+};
+
+class SimpleSprite : public godot::Sprite {
+	GODOT_CLASS(SimpleSprite, godot::Sprite)
+
+public:
+	static void _register_methods();
+
+	void _init();
+
+	void _process(double delta);
+};
