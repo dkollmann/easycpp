2f14ca64afcead43ba23d063e7a9b73ba375006c
Daniel Kollmann <dankolle@mail.de>
2021-06-12 21:31:12 +0200

    Added check to make sure that the addon is placed in the correct folder.

--- plugin.gd
+++ plugin.gd
@@ -5,6 +5,15 @@ var toolwindow :Control
 
 
 func _enter_tree():
+	var path = get_script().get_path()
+	
+	if path != "res://addons/easycpp/plugin.gd":
+		print("ERROR: The Easy C++ addon must be placed inside the folder \"res://addons/easycpp/\".")
+		
+		Directory.new().make_dir_recursive("res://addons/easycpp")
+		
+		return
+	
 	toolwindow = preload("res://addons/easycpp/scenes/tool_window.tscn").instance()
 	
 	toolwindow.editorbase = get_editor_interface().get_base_control()
